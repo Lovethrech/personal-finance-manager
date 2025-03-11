c6d34f2ec39386832e3f55981253d72ad20a77a9
Lovethrech <rachealdolapo45@gmail.com>
2025-03-11 11:30:09 +0100

    ...starting the landing-page views and components

--- src/components/landing-page/LandingPageMainCtn.vue
+++ src/components/landing-page/LandingPageMainCtn.vue
@@ -1,3 +1,7 @@
+<script setup>
+import SideNavMainCtn from '@/components/side-nav/SideNavMainCtn.vue';
+</script>
+
 <style scoped>
 .landing-page-main-ctn-main-ctn, .landing-page-main-ctn-mini-ctn{
     width:100%;
@@ -7,14 +11,14 @@
 
 }
 .landing-page-main-ctn-mini-ctn{
-
+    
 }
 </style>
 
 <template>
     <section class="landing-page-main-ctn-main-ctn">
         <div class="landing-page-main-ctn-mini-ctn">
-
+            <SideNavMainCtn></SideNavMainCtn>
         </div>
     </section>
 </template>
--- src/views/LandingPageView.vue
+++ src/views/LandingPageView.vue
@@ -1,10 +1,11 @@
 <script setup>
+import LandingPageMainCtn from '@/components/landing-page/LandingPageMainCtn.vue';
 </script>
 
 <template>  
     <main>
         <div class="landing-page-view-main-ctn">
-            
+            <LandingPageMainCtn></LandingPageMainCtn>
         </div>
     </main>
 </template>
