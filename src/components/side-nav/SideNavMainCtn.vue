a3e88e3952e317620f68c718d57225417a4b9a61
Lovethrech <rachealdolapo45@gmail.com>
2025-03-11 11:43:38 +0100

    ...starting the landing-page views and components

--- src/components/side-nav/SideNavMainCtn.vue
+++ src/components/side-nav/SideNavMainCtn.vue
@@ -1,7 +1,14 @@
+<script setup>
+import SideNavIcon from '@/components/side-nav/SideNavIcon.vue';
+</script>
+
 <template>
     <section class="side-nav-main-ctn-main-ctn">
         <div class="side-nav-main-ctn-mini-ctn">
-            
+            <!-- <div class="user"></div> -->
+            <section>
+                <SideNavIcon></SideNavIcon>
+            </section>
         </div>
     </section>
 </template>
