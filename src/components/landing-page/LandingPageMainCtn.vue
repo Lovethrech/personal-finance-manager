90cf6d74c130d114cfae2d99d8ac9a5c4abb7615
Lovethrech <rachealdolapo45@gmail.com>
2025-03-11 11:19:22 +0100

    ...starting the landing-page views and components

--- src/components/landing-page/LandingPageMainCtn.vue
+++ src/components/landing-page/LandingPageMainCtn.vue
@@ -1,7 +1,16 @@
+<style scoped>
+.landing-page-main-ctn-main-ctn{
+    
+}
+.landing-page-main-ctn-mini-ctn{
+
+}
+</style>
+
 <template>
     <div class="landing-page-main-ctn-main-ctn">
         <div class="landing-page-main-ctn-mini-ctn">
-            
+
         </div>
     </div>
 </template>
