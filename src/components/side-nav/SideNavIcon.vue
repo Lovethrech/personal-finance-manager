805fa4225bae06ecbbd0a10b80e866578275252e
Lovethrech <rachealdolapo45@gmail.com>
2025-03-13 14:59:28 +0100

    ...working on the side-nav components

--- src/components/side-nav/SideNavIcon.vue
+++ src/components/side-nav/SideNavIcon.vue
@@ -6,7 +6,7 @@ import SideNavIcons from "@/data/side-nav.json";
 .side-nav-icon{
 
 }
-.side-nav-icon-desc{
+.side-nav-icon-desc-ctn{
     color:white;
 }
 </style>
@@ -14,7 +14,11 @@ import SideNavIcons from "@/data/side-nav.json";
 <template>
     <RouterLink :to="SideNavIcons[1].link" class="link">
         <span class="side-nav-icon" :v-html="SideNavIcons[1].icon"></span>
-        <span class="side-nav-icon-desc" :v-html="SideNavIcons[1].name"></span>
+        <span class="side-nav-icon-desc-ctn">
+            <p class="side-nav-icon-desc">
+                {{SideNavIcons[1].name}}
+            </p>
+        </span>
         <!-- #93200B -->
         <!-- #C93005 -->
     </RouterLink>
