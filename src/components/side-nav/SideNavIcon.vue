bbf11557db07a52913b531d57df83ac347e5841f
Lovethrech <rachealdolapo45@gmail.com>
2025-03-13 14:38:53 +0100

    ...altering the side-nav json file

--- src/components/side-nav/SideNavIcon.vue
+++ src/components/side-nav/SideNavIcon.vue
@@ -1,6 +1,8 @@
 <template>
     <RouterLink to="/" class="link">
         <span class="side-nav-icon"></span>
-        <span></span>
+        <span class="side-nav-icon-desc"> 
+
+        </span>
     </RouterLink>
 </template>
