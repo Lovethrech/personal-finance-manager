164afa96c6ea058986402d513a3f4855b4102401
Lovethrech <rachealdolapo45@gmail.com>
2025-03-13 14:50:42 +0100

    ...working on the side-nav components

--- src/components/side-nav/SideNavIcon.vue
+++ src/components/side-nav/SideNavIcon.vue
@@ -1,14 +1,20 @@
 <script setup>
 import SideNavIcons from "@/data/side-nav.json";
+console.log()
 </script>
 
 <style scoped>
+.side-nav-icon{
 
+}
+.side-nav-icon-desc{
+    color:white;
+}
 </style>
 
 <template>
     <RouterLink to="/" class="link">
-        <span class="side-nav-icon"></span>
+        <span class="side-nav-icon" :v-html="SideNavIcons[1].icon"></span>
         <span class="side-nav-icon-desc" :v-html="SideNavIcons[1].name"></span>
         <!-- #93200B -->
         <!-- #C93005 -->
