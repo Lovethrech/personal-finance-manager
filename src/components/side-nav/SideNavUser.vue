0c55cd42acf2d6438f2bcbc2841beacd0fef5b8c
Lovethrech <rachealdolapo45@gmail.com>
2025-03-13 15:13:13 +0100

    ...working on the side-nav components

--- src/components/side-nav/SideNavUser.vue
+++ src/components/side-nav/SideNavUser.vue
@@ -1,7 +1,21 @@
+<script setup>
+import {defineProps} from "vue";
+
+const {name, icon, link}=defineProps(['name','icon', 'link']);
+</script>
+
+
 <template>
     <div class="side-nav-user-main-ctn">
-        <div class="side-nav-user-mini-ctn">
-            
-        </div>
+        <RouterLink :to="link">
+            <div class="side-nav-user-mini-ctn">
+                <div class="side-nav-user-mini-ctn-icon-ctn">
+
+                </div>
+                <p class="side-nav-user-mini-ctn-user-desc">
+                    {{name}}
+                </p>
+            </div>
+        </RouterLink>
     </div>
 </template>
