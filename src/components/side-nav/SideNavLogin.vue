aaaaa6dc31171944365aeb0c3512aa322671aa98
Lovethrech <rachealdolapo45@gmail.com>
2025-03-15 09:43:21 +0100

    ...working on the sideNav components

--- src/components/side-nav/SideNavLogin.vue
+++ src/components/side-nav/SideNavLogin.vue
@@ -1,8 +1,11 @@
+
+
 <template>
     <div class="side-nav-login-main-ctn">
-        <RouterLink to="/" class="link">
+        <RouterLink :to="link" class="link">
             <div class="side-nav-login-mini-ctn">
-
+                <div class="side-nav-login-icon-ctn" v-html="icon"></div>
+                <p class="side-nav-login-icon-ctn">{{name}}</p>
             </div>
         </RouterLink>
     </div>
