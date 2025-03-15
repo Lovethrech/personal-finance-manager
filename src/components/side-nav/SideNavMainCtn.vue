a0e14e31ea9bb0350d109d49649759ad3b7672ff
Lovethrech <rachealdolapo45@gmail.com>
2025-03-15 12:25:59 +0100

    ...fine-tuning the side-nav-icon components.

--- src/components/side-nav/SideNavMainCtn.vue
+++ src/components/side-nav/SideNavMainCtn.vue
@@ -1,12 +1,60 @@
 <script setup>
-import { useSideNavColorStore } from "@/stores/sideNavColor";
+// import { useSideNavColorStore } from "@/stores/sideNavColor";
 
 import SideNavIcons from "@/data/side-nav.json";
 import SideNavIcon from '@/components/side-nav/SideNavIcon.vue';
 import SideNavUser from "@/components/side-nav/SideNavUser.vue";
 import SideNavLogin from "@/components/side-nav/SideNavLogin.vue";
 
-const {dashboardActive, inventoryActive, orderActive, purchaseActive, reportingActive, supportActive, settingActive, sideNavDashboardClick, sideNavInventoryClick, sideNavOrderClick, sideNavPurchaseClick, sideNavReportingClick, sideNavSupportClick,sideNavSettingClick }= useSideNavColorStore();
+// const {dashboardActive, inventoryActive, orderActive, purchaseActive, reportingActive, supportActive, settingActive, sideNavDashboardClick, sideNavInventoryClick, sideNavOrderClick, sideNavPurchaseClick, sideNavReportingClick, sideNavSupportClick,sideNavSettingClick }= useSideNavColorStore();
+const dashboardActive=ref({
+        backgroundColor:"#000000",
+        borderRadius: "0"
+    });
+    const inventoryActive=ref({
+        backgroundColor:"#93200B",
+        borderRadius: "0 100px 0 0"
+    });
+    const orderActive=ref({
+        backgroundColor:"#93200B",
+        borderRadius: "0"
+    });
+    const purchaseActive=ref({
+        backgroundColor:"#93200B",
+        borderRadius: "0"
+    });
+    const reportingActive=ref({
+        backgroundColor:"#93200B",
+        borderRadius: "0"
+    });
+    const supportActive=ref({
+        backgroundColor:"#93200B",
+        borderRadius: "0"
+    });
+    const settingActive=ref({
+        backgroundColor:"#93200B",
+        borderRadius: "0"
+});
+
+const sideNavDashboardClick=()=>{
+    if(((inventoryActive.value.backgroundColor)==="#000000") / ((orderActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
+        dashboardActive.value.backgroundColor="#000000";
+        dashboardActive.value.borderRadius="0";
+        inventoryActive.value.backgroundColor="#93200B";
+        inventoryActive.value.borderRadius="0 100px 0 0";
+        orderActive.value.backgroundColor="#000000";
+        orderActive.value.borderRadius="0";
+    }
+};
+const sideNavInventoryClick=()=>{
+    if(((dashboardActive.value.backgroundColor)==="#000000") / ((orderActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
+        dashboardActive.value.backgroundColor="#93200B";
+        dashboardActive.value.borderRadius="0 0 100px 0";
+        inventoryActive.value.backgroundColor="#000000";
+        inventoryActive.value.borderRadius="0";
+        orderActive.value.borderRadius="0 100px 0 0";
+    }
+};
 </script>
 
 <style scoped>
