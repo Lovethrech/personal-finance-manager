ac3903c20e2ae3aca7387ee5c2def22895e717f3
Lovethrech <rachealdolapo45@gmail.com>
2025-03-15 12:43:00 +0100

    ...fine-tuning the side-nav-icon components.

--- src/stores/sideNavColor.js
+++ src/stores/sideNavColor.js
@@ -32,14 +32,7 @@ export const useSideNavColorStore = defineStore('sideNavColor', () => {
     });
 
     // const sideNavDashboardClick=()=>{
-    //     if(((inventoryActive.value.backgroundColor)==="#000000") / ((orderActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
-    //         dashboardActive.value.backgroundColor="#000000";
-    //         dashboardActive.value.borderRadius="0";
-    //         inventoryActive.value.backgroundColor="#93200B";
-    //         inventoryActive.value.borderRadius="0 100px 0 0";
-    //         orderActive.value.backgroundColor="#000000";
-    //         orderActive.value.borderRadius="0";
-    //     }
+        
     // };
     const sideNavClick=()=>{
         if(((dashboardActive.value.backgroundColor)==="#000000") / ((orderActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
@@ -47,58 +40,77 @@ export const useSideNavColorStore = defineStore('sideNavColor', () => {
             dashboardActive.value.borderRadius="0 0 100px 0";
             inventoryActive.value.backgroundColor="#000000";
             inventoryActive.value.borderRadius="0";
+            orderActive.value.backgroundColor="93200B";
             orderActive.value.borderRadius="0 100px 0 0";
         }
-    };
-    cons=()=>{
-        if(((dashboardActive.value.backgroundColor)==="#000000") / ((inventoryActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
+        else if(((inventoryActive.value.backgroundColor)==="#000000") / ((orderActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
+            dashboardActive.value.backgroundColor="#000000";
+            dashboardActive.value.borderRadius="0";
+            inventoryActive.value.backgroundColor="#93200B";
+            inventoryActive.value.borderRadius="0 100px 0 0";
+            orderActive.value.backgroundColor="#93200B";
+            orderActive.value.borderRadius="0";
+        }
+        else if(((dashboardActive.value.backgroundColor)==="#000000") / ((inventoryActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
             dashboardActive.value.backgroundColor="#93200B";
             inventoryActive.value.backgroundColor="#93200B";
             inventoryActive.value.borderRadius="0 0 100px 0";
             orderActive.value.backgroundColor="#000000"
             orderActive.value.borderRadius="0";
-            purchaseActive.value.borderRadius="0 100px 0 0"
+            purchaseActive.value.backgroundColor="93200B";
+            purchaseActive.value.borderRadius="0 100px 0 0";
         }
+        
     };
-    cons=()=>{
-        if(((dashboardActive.value.backgroundColor)==="#000000") / ((inventoryActive.value.backgroundColor)==="#000000") / ((orderActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
-            dashboardActive.value.backgroundColor="#93200B";
-            orderActive.value.backgroundColor="#93200B";
-            orderActive.value.borderRadius="0 0 100px 0";
-            purchaseActive.value.backgroundColor="#000000"
-            purchaseActive.value.borderRadius="0";
-            reportingActive.value.borderRadius="0 100px 0 0"
-        }
-    };
-    cons=()=>{
-        if(((dashboardActive.value.backgroundColor)==="#000000") / ((inventoryActive.value.backgroundColor)==="#000000") / ((orderActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
-            dashboardActive.value.backgroundColor="#93200B";
-            purchaseActive.value.backgroundColor="#93200B";
-            purchaseActive.value.borderRadius="0 0 100px 0";
-            reportingActive.value.backgroundColor="#000000"
-            reportingActive.value.borderRadius="0";
-            supportActive.value.borderRadius="0 100px 0 0"
-        }
-    };
-    cons=()=>{
-        if(((dashboardActive.value.backgroundColor)==="#000000") / ((inventoryActive.value.backgroundColor)==="#000000") / ((orderActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
-            dashboardActive.value.backgroundColor="#93200B";
-            reportingActive.value.backgroundColor="#93200B";
-            reportingActive.value.borderRadius="0 0 100px 0";
-            supportActive.value.backgroundColor="#000000"
-            supportActive.value.borderRadius="0";
-            settingActive.value.borderRadius="0 100px 0 0"
-        }
-    };
-    cons=()=>{
-        if(((dashboardActive.value.backgroundColor)==="#000000") / ((inventoryActive.value.backgroundColor)==="#000000") / ((orderActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000")){
-            dashboardActive.value.backgroundColor="#93200B";
-            supportActive.value.backgroundColor="#93200B";
-            supportActive.value.borderRadius="0 0 100px 0";
-            settingActive.value.backgroundColor="#000000"
-            settingActive.value.borderRadius="0";
-        }
-    };
+    // cons=()=>{
+    //     if(((dashboardActive.value.backgroundColor)==="#000000") / ((inventoryActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
+    //         dashboardActive.value.backgroundColor="#93200B";
+    //         inventoryActive.value.backgroundColor="#93200B";
+    //         inventoryActive.value.borderRadius="0 0 100px 0";
+    //         orderActive.value.backgroundColor="#000000"
+    //         orderActive.value.borderRadius="0";
+    //         purchaseActive.value.borderRadius="0 100px 0 0";
+    //     }
+    // };
+    // cons=()=>{
+    //     if(((dashboardActive.value.backgroundColor)==="#000000") / ((inventoryActive.value.backgroundColor)==="#000000") / ((orderActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
+    //         dashboardActive.value.backgroundColor="#93200B";
+    //         orderActive.value.backgroundColor="#93200B";
+    //         orderActive.value.borderRadius="0 0 100px 0";
+    //         purchaseActive.value.backgroundColor="#000000"
+    //         purchaseActive.value.borderRadius="0";
+    //         reportingActive.value.borderRadius="0 100px 0 0"
+    //     }
+    // };
+    // cons=()=>{
+    //     if(((dashboardActive.value.backgroundColor)==="#000000") / ((inventoryActive.value.backgroundColor)==="#000000") / ((orderActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
+    //         dashboardActive.value.backgroundColor="#93200B";
+    //         purchaseActive.value.backgroundColor="#93200B";
+    //         purchaseActive.value.borderRadius="0 0 100px 0";
+    //         reportingActive.value.backgroundColor="#000000"
+    //         reportingActive.value.borderRadius="0";
+    //         supportActive.value.borderRadius="0 100px 0 0"
+    //     }
+    // };
+    // cons=()=>{
+    //     if(((dashboardActive.value.backgroundColor)==="#000000") / ((inventoryActive.value.backgroundColor)==="#000000") / ((orderActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
+    //         dashboardActive.value.backgroundColor="#93200B";
+    //         reportingActive.value.backgroundColor="#93200B";
+    //         reportingActive.value.borderRadius="0 0 100px 0";
+    //         supportActive.value.backgroundColor="#000000"
+    //         supportActive.value.borderRadius="0";
+    //         settingActive.value.borderRadius="0 100px 0 0"
+    //     }
+    // };
+    // cons=()=>{
+    //     if(((dashboardActive.value.backgroundColor)==="#000000") / ((inventoryActive.value.backgroundColor)==="#000000") / ((orderActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000")){
+    //         dashboardActive.value.backgroundColor="#93200B";
+    //         supportActive.value.backgroundColor="#93200B";
+    //         supportActive.value.borderRadius="0 0 100px 0";
+    //         settingActive.value.backgroundColor="#000000"
+    //         settingActive.value.borderRadius="0";
+    //     }
+    // };
 
     return { dashboardActive, inventoryActive, orderActive, purchaseActive, reportingActive, supportActive, settingActive, sideNavClick }
 })
