f2a74e1971c25db7514a3bfe026f43dbb8ae5ef6
Lovethrech <rachealdolapo45@gmail.com>
2025-03-15 09:55:27 +0100

    ...working on the sideNav components

--- src/components/side-nav/SideNavLogin.vue
+++ src/components/side-nav/SideNavLogin.vue
@@ -5,7 +5,23 @@ const {name, icon, link}=defineProps(['name','icon', 'link']);
 </script>
 
 <style scoped>
-
+.side-nav-login-main-ctn{
+    width: 100%;
+    display:flex;
+    justify-content: center;
+}
+.side-nav-login-mini-ctn{
+    display:flex;
+    flex-direction:row;
+    color: #9e9998;
+}
+.side-nav-login-icon-ctn{
+    font-size:20px;
+}
+.side-nav-login-icon-desc{
+    text-transform:uppercase;
+    font-size:20px;
+}
 </style>
 
 <template>
@@ -13,7 +29,7 @@ const {name, icon, link}=defineProps(['name','icon', 'link']);
         <RouterLink :to="link" class="link">
             <div class="side-nav-login-mini-ctn">
                 <div class="side-nav-login-icon-ctn" v-html="icon"></div>
-                <p class="side-nav-login-icon-ctn">{{name}}</p>
+                <p class="side-nav-login-icon-desc">{{name}}</p>
             </div>
         </RouterLink>
     </div>
