6e408bd3992871abd600095dc3a8a15659f6b311
Lovethrech <rachealdolapo45@gmail.com>
2025-03-15 11:31:19 +0100

    ...fine-tuning the side-nav-icon components.

--- src/components/side-nav/SideNavMainCtn.vue
+++ src/components/side-nav/SideNavMainCtn.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { useProjectCardOverlayStore } from "@/stores/sideNavColor";
 
-const {projectCardOverlayShow}= useSideNavColorStore();
 import SideNavIcons from "@/data/side-nav.json";
 import SideNavIcon from '@/components/side-nav/SideNavIcon.vue';
 import SideNavUser from "@/components/side-nav/SideNavUser.vue";
 import SideNavLogin from "@/components/side-nav/SideNavLogin.vue";
+
+const {dashboardActiveBgColor, dashboardActiveBorderRadius, inventoryActiveBgColor, inventoryActiveBorderRadius, orderActiveBgColor, orderActiveBorderRadius, purchaseActiveBgColor, purchaseActiveBorderRadius, reportingActiveBgColor, reportingActiveBorderRadius, supportActiveBgColor, supportActiveBorderRadius, settingActiveBgColor, settingActiveBorderRadius }= useSideNavColorStore();
 </script>
 
 <style scoped>
@@ -25,12 +26,6 @@ import SideNavLogin from "@/components/side-nav/SideNavLogin.vue";
 }
 .side-nav-icon{
     background-color: #93200B;
-}
-.side-nav-icon:nth-child(1){
-    background-color:black;
-}
-.side-nav-icon:nth-child(2){
-    border-radius: 0 100px 0 0;
 }
 .login{
     position: absolute;
@@ -54,15 +49,14 @@ import SideNavLogin from "@/components/side-nav/SideNavLogin.vue";
             <br/>
             <section class="side-nav-icon-section">
                 <SideNavIcon
-                    class="side-nav-icon"
                     :name="SideNavIcons[1].name"
                     :icon="SideNavIcons[1].icon"
                     :link="SideNavIcons[1].link"
+                    :style="{}"
                 >
                 </SideNavIcon>
 
                 <SideNavIcon
-                    class="side-nav-icon"
                     :name="SideNavIcons[2].name"
                     :icon="SideNavIcons[2].icon"
                     :link="SideNavIcons[2].link"
@@ -70,7 +64,6 @@ import SideNavLogin from "@/components/side-nav/SideNavLogin.vue";
                 </SideNavIcon>
 
                 <SideNavIcon
-                    class="side-nav-icon"
                     :name="SideNavIcons[3].name"
                     :icon="SideNavIcons[3].icon"
                     :link="SideNavIcons[3].link"
@@ -78,7 +71,6 @@ import SideNavLogin from "@/components/side-nav/SideNavLogin.vue";
                 </SideNavIcon>
 
                 <SideNavIcon
-                    class="side-nav-icon"
                     :name="SideNavIcons[4].name"
                     :icon="SideNavIcons[4].icon"
                     :link="SideNavIcons[4].link"
@@ -86,7 +78,6 @@ import SideNavLogin from "@/components/side-nav/SideNavLogin.vue";
                 </SideNavIcon>
 
                 <SideNavIcon
-                    class="side-nav-icon"
                     :name="SideNavIcons[5].name"
                     :icon="SideNavIcons[5].icon"
                     :link="SideNavIcons[5].link"
@@ -94,7 +85,6 @@ import SideNavLogin from "@/components/side-nav/SideNavLogin.vue";
                 </SideNavIcon>
 
                 <SideNavIcon
-                    class="side-nav-icon"
                     :name="SideNavIcons[6].name"
                     :icon="SideNavIcons[6].icon"
                     :link="SideNavIcons[6].link"
@@ -102,7 +92,6 @@ import SideNavLogin from "@/components/side-nav/SideNavLogin.vue";
                 </SideNavIcon>
 
                 <SideNavIcon
-                    class="side-nav-icon"
                     :name="SideNavIcons[7].name"
                     :icon="SideNavIcons[7].icon"
                     :link="SideNavIcons[7].link"
