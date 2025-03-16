58e7e72eadbfe4ff90cf9af41d5aa73b0676fe2c
Lovethrech <rachealdolapo45@gmail.com>
2025-03-16 13:57:33 +0100

    ...fine-tuning the side-nav-icon components.

--- src/stores/sideNavColor.js
+++ src/stores/sideNavColor.js
@@ -42,6 +42,7 @@ export const useSideNavColorStore = defineStore('sideNavColor', () => {
     // reporting 
     // support 
     // setting 
+    // #93200B 
 
 
     const sideNavClick=()=>{
@@ -61,20 +62,36 @@ export const useSideNavColorStore = defineStore('sideNavColor', () => {
             settingActive.value.backgroundColor="93200B";
             settingActive.value.borderRadius="0";
         }
-        else if(((dashboardActive.value.backgroundColor)==="#000000") / ((orderActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
+        else if(((dashboardActive.value.backgroundColor)==="#000000") / ((inventoryActive.value.backgroundColor)==="#000000") / ((purchaseActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
             dashboardActive.value.backgroundColor="#93200B";
-            dashboardActive.value.borderRadius="0 0 100px 0";
-            inventoryActive.value.backgroundColor="#000000";
-            inventoryActive.value.borderRadius="0";
-            orderActive.value.backgroundColor="93200B";
-            orderActive.value.borderRadius="0 100px 0 0"; 
-            purchaseActive.value.backgroundColor="93200B";
-            purchaseActive.value.borderRadius="0";
-            reportingActive.value.backgroundColor="93200B";
+            dashboardActive.value.borderRadius="0";
+            inventoryActive.value.backgroundColor="#93200B";
+            inventoryActive.value.borderRadius="0 0 100px 0";
+            orderActive.value.backgroundColor="#000000";
+            orderActive.value.borderRadius="0"; 
+            purchaseActive.value.backgroundColor="#93200B";
+            purchaseActive.value.borderRadius="0 100px 0 0";
+            reportingActive.value.backgroundColor="#93200B";
             reportingActive.value.borderRadius="0";
-            supportActive.value.backgroundColor="93200B";
+            supportActive.value.backgroundColor="#93200B";
             supportActive.value.borderRadius="0";
-            settingActive.value.backgroundColor="93200B";
+            settingActive.value.backgroundColor="#93200B";
+            settingActive.value.borderRadius="0";
+        }
+        else if(((dashboardActive.value.backgroundColor)==="#000000") / ((inventoryActive.value.backgroundColor)==="#000000") / ((orderActive.value.backgroundColor)==="#000000") / ((reportingActive.value.backgroundColor)==="#000000") / ((supportActive.value.backgroundColor)==="#000000") / ((settingActive.value.backgroundColor)==="#000000")){
+            dashboardActive.value.backgroundColor="#93200B";
+            dashboardActive.value.borderRadius="0";
+            inventoryActive.value.backgroundColor="#93200B";
+            inventoryActive.value.borderRadius="0";
+            orderActive.value.backgroundColor="#93200B";
+            orderActive.value.borderRadius="0 100px 0 0"; 
+            purchaseActive.value.backgroundColor="#000000";
+            purchaseActive.value.borderRadius="0";
+            reportingActive.value.backgroundColor="#93200B";
+            reportingActive.value.borderRadius="0 100px 0 0";
+            supportActive.value.backgroundColor="#93200B";
+            supportActive.value.borderRadius="0";
+            settingActive.value.backgroundColor="#93200B";
             settingActive.value.borderRadius="0";
         }
         
