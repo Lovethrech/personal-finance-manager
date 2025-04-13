81cb0ab0320d531b438230952b3e98cc7b91a8d9
Lovethrech <rachealdolapo45@gmail.com>
2025-04-13 18:00:37 +0100

    ...created top-main-nav container.

---
+++ src/components/MainTopNav.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="main-top-nav-main-ctn">
+        <div class="main-top-nav-mini-ctn">
+            
+        </div>
+    </div>
+    <!-- welcome user
+    search...
+    notification bell with small dot ontop  -->
+</template>
