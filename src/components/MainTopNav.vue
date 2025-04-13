dbc10721cf75015f0f25532ccc93095a73c25805
Lovethrech <rachealdolapo45@gmail.com>
2025-04-13 18:02:54 +0100

    ...created top-main-nav container.

--- src/components/MainTopNav.vue
+++ src/components/MainTopNav.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="main-top-nav-main-ctn">
         <div class="main-top-nav-mini-ctn">
-            
+            <h1 class="user-welcome-msg">
+                Welcome Rachael!
+            </h1>
+            <div class="search-box-ctn">
+                <input type="text" class="search-box">
+            </div>
         </div>
     </div>
     <!-- welcome user
