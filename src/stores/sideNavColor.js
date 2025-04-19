08d01739073d069c2d10af662874cf396dc0cda3
Lovethrech <rachealdolapo45@gmail.com>
2025-04-19 11:09:36 +0100

    ...working on side-nav containers.

--- src/stores/sideNavColor.js
+++ src/stores/sideNavColor.js
@@ -38,6 +38,7 @@ export const useSideNavColorStore = defineStore('sideNavColor', () => {
         dashboardActive.value.borderRadius = "0";
         inventoryActive.value.borderRadius = "0 100px 0 0";
         profileBorderStyle.value="0 0 100px 0";
+        loginBorderStyle.value="0";
     };
 
     const sideNavInventoryClick = () => {
