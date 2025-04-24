fb04e17ad33aec1c1a88f0d45297f1acca721b3a
Lovethrech <rachealdolapo45@gmail.com>
2025-04-24 10:49:37 +0100

    ...starting with the landing-page-overview section.

--- src/components/landing-page/LandingPageOverviewMainCtn.vue
+++ src/components/landing-page/LandingPageOverviewMainCtn.vue
@@ -1,5 +1,7 @@
 <template>
     <section class="landing-page-overview-main-ctn">
-        
+        <div class="landing-page-overview-mini-ctn">
+            
+        </div>
     </section>
 </template>
