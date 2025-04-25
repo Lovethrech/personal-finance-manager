04885e9bd17dcccc792f7f3e69e5a48977d7d93b
Lovethrech <rachealdolapo45@gmail.com>
2025-04-25 11:11:59 +0100

    ...working on landing-page-overview section.

--- src/components/landing-page/LandingPageOverviewMainCtn.vue
+++ src/components/landing-page/LandingPageOverviewMainCtn.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="landing-page-overview-main-ctn">
         <div class="landing-page-overview-mini-ctn">
-
+            <h3 class="page-title">Overview</h3>
         </div>
     </section>
 </template>
@@ -12,8 +12,14 @@
     width:80vw;
     height:200px;
     margin: 10px 3vw;
+    padding:20px;
 }
 .landing-page-overview-mini-ctn{
 
+}
+.page-title{
+    color:#fff;
+    font-size: 26px;
+    text-transform: uppercase;
 }
 </style>
