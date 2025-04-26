bb66a60ff7815cff108c9d7237f52877eee3f94a
Lovethrech <rachealdolapo45@gmail.com>
2025-04-26 12:12:47 +0100

    ...working on landing-page-overview container

--- src/components/landing-page/LandingPageOverviewBoxMainCtn.vue
+++ src/components/landing-page/LandingPageOverviewBoxMainCtn.vue
@@ -1,11 +1,11 @@
 <script setup>
-
+import LandingPageOverviewBox from './LandingPageOverviewBox.vue';
 </script>
 
 <template>
     <div class="landing-page-overview-box-main-ctn">
         <div class="landing-page-overview-box-mini-ctn">
-            
+            <LandingPageOverviewBox></LandingPageOverviewBox>
         </div>
     </div>
 </template>
