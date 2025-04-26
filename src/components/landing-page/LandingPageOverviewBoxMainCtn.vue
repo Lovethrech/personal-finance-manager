73a19b54b1af4b646a0002954eaffac1528af055
Lovethrech <rachealdolapo45@gmail.com>
2025-04-26 11:26:36 +0100

    ...working on landing-page-overview section.

--- src/components/landing-page/LandingPageOverviewBoxMainCtn.vue
+++ src/components/landing-page/LandingPageOverviewBoxMainCtn.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="landing-page-overview-box-main-ctn">
-        
+        <div class="landing-page-overview-box-mini-ctn">
+            
+        </div>
     </div>
 </template>
