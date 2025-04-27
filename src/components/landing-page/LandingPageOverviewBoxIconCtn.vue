9932189ec636ef2ea73e1c981d0ee4cbea8e642a
Lovethrech <rachealdolapo45@gmail.com>
2025-04-27 12:25:34 +0100

    ...working on landing-page-overview container

--- src/components/landing-page/LandingPageOverviewBoxIconCtn.vue
+++ src/components/landing-page/LandingPageOverviewBoxIconCtn.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="landing-page-overview-box-icon-ctn">
-        
+
     </div>
 </template>
+
+<style scoped>
+.landing-page-overview-box-icon-ctn{
+    width:20px;
+    height:20px;
+}
+</style>
