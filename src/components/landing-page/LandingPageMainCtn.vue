c900cfd8b6be4ec5a85682d8a1b0f9cf69e7ab0a
Lovethrech <rachealdolapo45@gmail.com>
2025-05-04 19:05:31 +0100

    ...working on the landing0-page-overview container.

--- src/components/landing-page/LandingPageMainCtn.vue
+++ src/components/landing-page/LandingPageMainCtn.vue
@@ -5,11 +5,11 @@ import LandingPageOverviewMainCtn from "@/components/landing-page/LandingPageOve
 </script>
 
 <style scoped>
-.landing-page-main-ctn-main-ctn{
+.landing-page-main-ctn-main-ctn, .landing-page-main-ctn-mini-ctn{
+    width:100%;
     height:100%;
 }
 .landing-page-main-ctn-mini-ctn{
-    width:100%;
     display:flex;
     flex-direction: row;
 }
