428e3408b6b5f19026663040d1278728a4aae926
Lovethrech <rachealdolapo45@gmail.com>
2025-05-10 14:54:13 +0100

    ...working on the landing0-page-overview container.

--- src/components/landing-page/LandingPageOverviewBox.vue
+++ src/components/landing-page/LandingPageOverviewBox.vue
@@ -33,6 +33,8 @@ const {boxIcon, boxName, boxNo}=defineProps(['boxIcon', 'boxName', 'boxNo']);
     justify-content: space-between;
 }
 @media screen and (max-width:700px){
-    
+    .landing-page-overview-box{
+        width:800px;
+    }
 }
 </style>
