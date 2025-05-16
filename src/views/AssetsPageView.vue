2320aa6afd059a026e2160d12983d9f2357a1aa7
Lovethrech <rachealdolapo45@gmail.com>
2025-05-16 16:23:21 +0100

    ...creating other necessary page in view folder.

--- src/views/AssetsPageView.vue
+++ src/views/AssetsPageView.vue
@@ -1,5 +1,18 @@
-<template>
-    <div class="assets">
-        
-    </div>
+<script setup>
+import LandingPageMainCtn from '@/components/landing-page/LandingPageMainCtn.vue';
+</script>
+
+<template>  
+    <main>
+        <div class="landing-page-view-main-ctn">
+            <LandingPageMainCtn></LandingPageMainCtn>
+        </div>
+    </main>
 </template>
+
+<style scoped>
+.landing-page-view-main-ctn{
+    width:100%;
+    overflow-x: hidden;
+}
+</style>
