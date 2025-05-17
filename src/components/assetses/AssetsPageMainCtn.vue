0469a7999749f0f56db8156cb4c5e0d764a9f896
Lovethrech <rachealdolapo45@gmail.com>
2025-05-17 16:35:01 +0100

    ...creating other necessary page in view folder.

--- src/components/assetses/AssetsPageMainCtn.vue
+++ src/components/assetses/AssetsPageMainCtn.vue
@@ -1,5 +1,36 @@
+<script setup>
+import SideNavMainCtn from '@/components/side-nav/SideNavMainCtn.vue';
+import MainTopNav from './MainTopNav.vue';
+import LandingPageOverviewMainCtn from "@/components/landing-page/LandingPageOverviewMainCtn.vue";
+</script>
+
+<style scoped>
+.landing-page-main-ctn-main-ctn, .landing-page-main-ctn-mini-ctn{
+    width:100%;
+    height:100%;
+}
+.landing-page-main-ctn-mini-ctn{
+    display:flex;
+    flex-direction: row;
+}
+main{
+    width:88%;
+}
+@media screen and (max-width:1060px) {
+    main{
+        width:80vw;
+    }
+}
+</style>
+
 <template>
-    <div class="assets-page-main-ctn">
-        
-    </div>
+    <section class="landing-page-main-ctn-main-ctn">
+        <div class="landing-page-main-ctn-mini-ctn">
+            <SideNavMainCtn></SideNavMainCtn>
+            <main>
+                <MainTopNav></MainTopNav>
+                <LandingPageOverviewMainCtn></LandingPageOverviewMainCtn>
+            </main>
+        </div>
+    </section>
 </template>
