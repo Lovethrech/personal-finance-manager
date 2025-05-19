2828064a19436d00934b0dc32b989e74bb2f0ab0
Lovethrech <rachealdolapo45@gmail.com>
2025-05-19 16:48:41 +0100

    ...working other necessary page in view folder.

--- src/views/ExpensesPageView.vue
+++ src/views/ExpensesPageView.vue
@@ -1,5 +1,18 @@
+<script setup>
+import AssetsPageMainCtn from '@/components/assetses/AssetsPageMainCtn.vue';
+</script>
+
 <template>
-    <div class="expenses">
-        
-    </div>
+    <main>
+        <div class="assets-page-main-ctn">
+            <AssetsPageMainCtn></AssetsPageMainCtn>
+        </div>
+    </main>
 </template>
+
+<style scoped>
+.assets-page-main-ctn{
+    width:100%;
+    overflow-x: hidden;
+}
+</style>
