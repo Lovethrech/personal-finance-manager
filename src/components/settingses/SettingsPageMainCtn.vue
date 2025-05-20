b3e1e754b5c379254169e9cb3693fcfba86b28ca
Lovethrech <rachealdolapo45@gmail.com>
2025-05-20 16:51:50 +0100

    ...working other necessary page in view folder.

--- src/components/settingses/SettingsPageMainCtn.vue
+++ src/components/settingses/SettingsPageMainCtn.vue
@@ -1,3 +1,34 @@
+<script setup>
+import SideNavMainCtn from '@/components/side-nav/SideNavMainCtn.vue';
+import MainTopNav from '@/components/landing-page/MainTopNav.vue';
+</script>
+
+<style scoped>
+.assets-page-main-ctn-main-ctn, .assets-page-main-ctn-mini-ctn{
+    width:100%;
+    height:100%;
+}
+.assets-page-main-ctn-mini-ctn{
+    display:flex;
+    flex-direction: row;
+}
+main{
+    width:88%;
+}
+@media screen and (max-width:1060px) {
+    main{
+        width:80vw;
+    }
+}
+</style>
+
 <template>
-    <div class="set-main-ctn"></div>
+    <section class="assets-page-main-ctn-main-ctn">
+        <div class="assets-page-main-ctn-mini-ctn">
+            <SideNavMainCtn></SideNavMainCtn>
+            <main>
+                <MainTopNav></MainTopNav>
+            </main>
+        </div>
+    </section>
 </template>
