bc702f59e84f1ed417ff51fe8d2ad9cbf49ac279
Lovethrech <rachealdolapo45@gmail.com>
2025-05-21 16:59:17 +0100

    ...working other necessary page in view folder.

--- src/views/SettingsPageView.vue
+++ src/views/SettingsPageView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import SettingsPageMainCtn from '@/components/settings/SettingsPageMainCtn.vue';
+import SettingsPageMainCtn from '@/components/settingses/SettingsPageMainCtn.vue';
 </script>
 
 <template>
