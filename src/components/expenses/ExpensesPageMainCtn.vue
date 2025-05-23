a7480151458acf6c969300736dc0c4a5256e4359
Lovethrech <rachealdolapo45@gmail.com>
2025-05-23 12:15:11 +0100

    ...working other necessary page in view folder.

--- src/components/expenses/ExpensesPageMainCtn.vue
+++ src/components/expenses/ExpensesPageMainCtn.vue
@@ -1,6 +1,6 @@
 <script setup>
 import SideNavMainCtn from '@/components/side-nav/SideNavMainCtn.vue';
-import MainTopNav from '@/components/landing-page/MainTopNav.vue';
+import MainTopNav from './MainTopNav.vue';
 </script>
 
 <style scoped>
