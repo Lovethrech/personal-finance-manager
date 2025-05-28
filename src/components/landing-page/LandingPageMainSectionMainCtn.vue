9050c545f8aa405b97e44b735c16f75ad7eece03
Lovethrech <rachealdolapo45@gmail.com>
2025-05-28 16:20:36 +0100

    ...working other landing page overview sub folders.

--- src/components/landing-page/LandingPageMainSectionMainCtn.vue
+++ src/components/landing-page/LandingPageMainSectionMainCtn.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="landing-page-main-section-main-ctn">
-        
+        <div class="landing-page-main-section-mini-ctn">
+            
+        </div>
     </div>
 </template>
