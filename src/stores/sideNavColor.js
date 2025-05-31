07ef7804e85a59e3c01ca362aa824e7e4cd8c3c4
Lovethrech <rachealdolapo45@gmail.com>
2025-05-31 17:12:42 +0100

    ...finetuning all available components.

--- src/stores/sideNavColor.js
+++ src/stores/sideNavColor.js
@@ -11,7 +11,7 @@ export const useSideNavColorStore = defineStore('sideNavColor', () => {
     const inventoryActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0", color: defaultColor });
     const orderActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0", color: defaultColor });
     const purchaseActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0", color: defaultColor });
-    const reportingActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0" });
+    const reportingActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0", color: defaultColor });
     const supportActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0" });
     const settingActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0" });
     const profileBorderStyle=ref({borderRadius: "0 0 100px 0"});
