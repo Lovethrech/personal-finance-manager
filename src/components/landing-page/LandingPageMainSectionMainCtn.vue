b44b76d00afe5726c5bc7501c1012629a0c6666a
Lovethrech <rachealdolapo45@gmail.com>
2025-06-02 20:18:16 +0100

    ...finetuning all available components.

--- src/components/landing-page/LandingPageMainSectionMainCtn.vue
+++ src/components/landing-page/LandingPageMainSectionMainCtn.vue
@@ -12,7 +12,7 @@ import LandingPageOverviewMainCtn from "./LandingPageOverviewMainCtn.vue";
 
 <style scoped>
 .landing-page-main-section-main-ctn{
-    background-color: #fff;
+    background-color: transparent;
     width:100%;
 }
 .landing-page-main-section-mini-ctn{
