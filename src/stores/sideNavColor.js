439becae15230bb89d38e4935310a34aa5242882
Lovethrech <rachealdolapo45@gmail.com>
2025-06-04 20:44:35 +0100

    ...finetuning all available components.

--- src/stores/sideNavColor.js
+++ src/stores/sideNavColor.js
@@ -9,7 +9,7 @@ export const useSideNavColorStore = defineStore('sideNavColor', () => {
     const activeFontWeight= "800";
     const defaultFontWeight="200";
 
-    const dashboardActive = ref({ backgroundColor: activeBgColor, borderRadius: "0", color: activeColor, fontWeight:activeFontWeight });
+    const dashboardActive = ref({ backgroundColor: activeBgColor, borderRadius: "0", color: activeColor, dashboardFontWeight:activeFontWeight });
     const inventoryActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0 100px 0 0", color: defaultColor, fontWeight:defaultFontWeight });
     const orderActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0", color: defaultColor, fontWeight:defaultFontWeight });
     const purchaseActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0", color: defaultColor, fontWeight:defaultFontWeight });
