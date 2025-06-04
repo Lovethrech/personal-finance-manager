8b80703acc2742ebf384bb177e510dc4f184ad90
Lovethrech <rachealdolapo45@gmail.com>
2025-06-04 20:45:36 +0100

    ...finetuning all available components.

--- src/stores/sideNavColor.js
+++ src/stores/sideNavColor.js
@@ -9,9 +9,9 @@ export const useSideNavColorStore = defineStore('sideNavColor', () => {
     const activeFontWeight= "800";
     const defaultFontWeight="200";
 
-    const dashboardActive = ref({ backgroundColor: activeBgColor, borderRadius: "0", color: activeColor, dashboardFontWeight:activeFontWeight });
-    const inventoryActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0 100px 0 0", color: defaultColor, inventoryFontWeight:defaultFontWeight });
-    const orderActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0", color: defaultColor, orderFontWeight:defaultFontWeight });
+    const dashboardActive = ref({ backgroundColor: activeBgColor, borderRadius: "0", color: activeColor, fontWeight:activeFontWeight });
+    const inventoryActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0 100px 0 0", color: defaultColor, fontWeight:defaultFontWeight });
+    const orderActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0", color: defaultColor, fontWeight:defaultFontWeight });
     const purchaseActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0", color: defaultColor, fontWeight:defaultFontWeight });
     const reportingActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0", color: defaultColor, fontWeight:defaultFontWeight });
     const supportActive = ref({ backgroundColor: defaultBgColor, borderRadius: "0", color: defaultColor, fontWeight:defaultFontWeight });
