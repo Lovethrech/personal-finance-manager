2df07481ea474a8e62a9e2eaa05e9201d7b61d33
Lovethrech <rachealdolapo45@gmail.com>
2025-06-11 21:47:47 +0100

    ...finetuning all available components.

--- src/stores/sideNavColor.js
+++ src/stores/sideNavColor.js
@@ -125,7 +125,7 @@ export const useSideNavColorStore = defineStore('sideNavColor', () => {
         purchaseActive.value.fontWeight=activeFontWeight;
         orderActive.value.borderRadius = "0 0 100px 0";
         orderActive.value.borderBottom="5px outset #723a3052";
-        orderActive.value.borderTop="5px outset #723a3052";
+        reportingActive.value.borderTop="5px outset #723a3052";
         reportingActive.value.borderRadius = "0 100px 0 0";
         profileBorderStyle.value.borderRadius="0";
         profileBorderStyle.value.borderBottom="0";
