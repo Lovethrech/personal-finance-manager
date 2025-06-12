fafdf55db8ba57a1696977d9b2d6815be9200d2c
Lovethrech <rachealdolapo45@gmail.com>
2025-06-12 21:59:16 +0100

    ...finetuning all available components.

--- src/stores/sideNavColor.js
+++ src/stores/sideNavColor.js
@@ -143,7 +143,7 @@ export const useSideNavColorStore = defineStore('sideNavColor', () => {
         profileBorderStyle.value.borderBottom="0";
     };
 
-    const sideNavSupportClick = () => {
+    const sideNavInvestmentClick = () => {
         resetColors();
         investmentActive.value.backgroundColor = activeBgColor;
         investmentActive.value.borderRadius = "0";
@@ -168,5 +168,5 @@ export const useSideNavColorStore = defineStore('sideNavColor', () => {
         loginBorderStyle.value.borderTop="5px outset #723a3052";
     };
 
-    return {dashboardActive, assetActive, incomeActive, expenseActive, savingActive, investmentActive, settingActive, profileBorderStyle, loginBorderStyle, sideNavDashboardClick, sideNavAssetClick, sideNavIncomeClick, sideNavExpenseClick, sideNavSavingClick, sideNavSupportClick, sideNavSettingClick};
+    return {dashboardActive, assetActive, incomeActive, expenseActive, savingActive, investmentActive, settingActive, profileBorderStyle, loginBorderStyle, sideNavDashboardClick, sideNavAssetClick, sideNavIncomeClick, sideNavExpenseClick, sideNavSavingClick, sideNavInvestmentClick, sideNavSettingClick};
 });
