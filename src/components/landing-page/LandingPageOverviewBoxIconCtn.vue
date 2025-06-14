917bf66b469412658dae77dc80a5629d10dcb8fa
Lovethrech <rachealdolapo45@gmail.com>
2025-06-14 16:01:04 +0100

    ...finetuning all available components.

--- src/components/landing-page/LandingPageOverviewBoxIconCtn.vue
+++ src/components/landing-page/LandingPageOverviewBoxIconCtn.vue
@@ -7,29 +7,6 @@ const {boxIcon}=defineProps(['boxIcon']);
 <template>
     <div class="landing-page-overview-box-icon-ctn">
         <div class="landing-page-overview-box-icon-ctn-main-ctn" v-html="boxIcon"></div>
-<!-- @media screen and (max-width:750px)  and (min-width:551px){
-    .landing-page-overview-box-icon-ctn-main-ctn{
-        width:50px;
-        height:100%;
-        font-size:26px;
-    }
-    .landing-page-overview-box-icon-ctn{
-        width:50%;
-        height:40px;
-    }
-}
-@media screen and (max-width: 550px) {
-    .landing-page-overview-box-icon-ctn-main-ctn{
-        width:30px;
-        height:100%;
-        font-size:20px;
-    }
-    .landing-page-overview-box-icon-ctn{
-        width:30%;
-        height:30px;
-    }
-} -->
-
     </div>
 </template>
 
